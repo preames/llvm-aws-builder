8be3fb6ba38528eec822b6601d2750b484b0c82d
Philip Reames <listmail@philipreames.com>
2020-12-14 14:54:42 -0800

    replace master w/main

--- aws-builder-ubuntu-incremental.sh
+++ aws-builder-ubuntu-incremental.sh
@@ -3,7 +3,7 @@ set -e
 pushd ~/llvm-repo/llvm-project/llvm
 echo "Starting incremental build with fresh patch"
 date
-git checkout -f master
+git checkout -f main
 git clean -fd
 git pull --ff-only
 patch -p2 < ~/BuildOnAWS.diff
--- aws-builder-upload.sh
+++ aws-builder-upload.sh
@@ -1,6 +1,6 @@
 set -e
 set -x
-git diff origin/master > BuildOnAWS.diff
+git diff origin/main > BuildOnAWS.diff
 scp BuildOnAWS.diff ubuntu@${AWS_BUILDER_URL}:~/
 rm BuildOnAWS.diff
 ssh ubuntu@${AWS_BUILDER_URL} './aws-builder-ubuntu-incremental.sh'
