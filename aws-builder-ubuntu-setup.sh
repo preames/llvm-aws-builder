ff4f66ddcca3f560011c30331c6004ba90c2ead5
Philip Reames <listmail@philipreames.com>
2020-12-14 12:14:07 -0800

    Update scripts for modern ami types, and adjust a couple minor usage details

--- aws-builder-ubuntu-setup.sh
+++ aws-builder-ubuntu-setup.sh
@@ -1,15 +1,19 @@
 # install relevant packages
 sudo apt-get update
-sudo apt-get --assume-yes install emacs man-db libc6-dev dpkg-dev make build-essential binutils binutils-dev gcc g++ autoconf python git clang
+sudo apt-get --assume-yes install emacs man-db libc6-dev dpkg-dev make build-essential binutils binutils-dev gcc g++ autoconf python git clang cmake
 
 # install a new enough cmake (from source)
-mkdir cmake
-pushd cmake
-wget https://cmake.org/files/v3.8/cmake-3.8.0-Linux-x86_64.sh
-chmod u+x cmake-3.8.0-Linux-x86_64.sh
-./cmake-3.8.0-Linux-x86_64.sh --skip-license
-export PATH=~/cmake/bin/:$PATH
-popd
+# NOTE: Ubuntu 20.04 currently has a recent enough one in repo
+#mkdir cmake
+#pushd cmake
+#wget https://cmake.org/files/v3.8/cmake-3.8.0-Linux-x86_64.sh
+#chmod u+x cmake-3.8.0-Linux-x86_64.sh
+#./cmake-3.8.0-Linux-x86_64.sh --skip-license
+#export PATH=~/cmake/bin/:$PATH
+#popd
+
+#
+chmod u+x aws-*.sh
 
 # Setup the source/build tree
 mkdir llvm-repo
@@ -20,4 +24,4 @@ cd build/
 cmake ../llvm-project/llvm/ -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=On -DLLVM_TARGETS_TO_BUILD=X86
 
 # Do the initial full build
-time make -j31
+# time make -j31
--- aws-builder-upload.sh
+++ aws-builder-upload.sh
@@ -3,6 +3,4 @@ set -x
 git diff origin/master > BuildOnAWS.diff
 scp BuildOnAWS.diff ubuntu@${AWS_BUILDER_URL}:~/
 rm BuildOnAWS.diff
-scp aws-*.sh ubuntu@${AWS_BUILDER_URL}:~/
-ssh ubuntu@${AWS_BUILDER_URL} 'chmod u+x aws-builder-ubuntu-incremental.sh'
 ssh ubuntu@${AWS_BUILDER_URL} './aws-builder-ubuntu-incremental.sh'
