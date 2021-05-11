9897aba38150a826ef1a0c984dc3b5c89d4e4582
Philip Reames <listmail@philipreames.com>
2021-05-11 10:50:33 -0700

    Various tweaks from daily usage

--- aws-builder-ubuntu-incremental.sh
+++ aws-builder-ubuntu-incremental.sh
@@ -2,15 +2,20 @@ set -x
 set -e
 echo "Starting incremental build with fresh patch"
 date
-# ensure shutdown 120 minutes after last build attempt
+# ensure shutdown 180 minutes after last build attempt
 sudo shutdown -c
-sudo shutdown -P +120
-pushd ~/llvm-repo/llvm-project/llvm
+sudo shutdown -P +180
+pushd ~/llvm-repo/llvm-project
+# Replace the next four lines with the commented out ones for a much
+# faster incremental rebuild at the risk of missing rebase problems.
+# git diff > ~/LastDiff.diff
+# interdiff ~/LastDiff.diff ~/BuildOnAWS.diff > ~/Delta.diff
+# patch -p2 < ~/Delta.diff
 git checkout -f main
 git clean -fd
 git pull --ff-only
-patch -p2 < ~/BuildOnAWS.diff
-time nice -n 19 make -j31 -C ../../build ${@:1}
+patch -p1 < ~/BuildOnAWS.diff
+time nice -n 19 make -j31 -C ../build ${@:1}
 popd
 echo "Finished incremental build"
 date
--- aws-builder-ubuntu-setup.sh
+++ aws-builder-ubuntu-setup.sh
@@ -1,10 +1,10 @@
-# ensure shutdown 120 minutes after last build attempt
+# ensure shutdown 180 minutes after last build attempt
 sudo shutdown -c
-sudo shutdown -P +120
+sudo shutdown -P +180
 
 # install relevant packages
 sudo apt-get update
-sudo apt-get --assume-yes install emacs man-db libc6-dev dpkg-dev make build-essential binutils binutils-dev gcc g++ autoconf python git clang cmake
+sudo apt-get --assume-yes install emacs man-db libc6-dev dpkg-dev make build-essential binutils binutils-dev gcc g++ autoconf python git clang cmake patchutils
 
 # install a new enough cmake (from source)
 # NOTE: Ubuntu 20.04 currently has a recent enough one in repo
@@ -25,7 +25,7 @@ mkdir llvm-repo/build
 cd llvm-repo
 git clone --depth 500 https://github.com/llvm/llvm-project.git
 cd build/
-cmake ../llvm-project/llvm/ -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=On -DLLVM_TARGETS_TO_BUILD=X86
+cmake ../llvm-project/llvm/ -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=On -DLLVM_TARGETS_TO_BUILD=X86 ##-DLLVM_ENABLE_PROJECTS=clang
 
 # Do the initial full build
-# time make -j31
+time nice -n 19 make -j31
