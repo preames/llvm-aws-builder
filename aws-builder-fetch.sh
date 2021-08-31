87910a61d7e93cf1f629aad583ed4ee0a907ad69
Philip Reames <listmail@philipreames.com>
2021-08-31 12:28:16 -0700

    Add a fetch wrapper

---
+++ aws-builder-fetch.sh
@@ -0,0 +1,9 @@
+set -e
+#set -x
+if [ "$#" -ne 2 ]; then
+    echo "aws-builder-fetch.sh <toolname> <local-dest>"
+    echo "  use \"{tool1,tool2}\" for multiple tools"
+    exit -1
+fi
+
+scp llvm-builder:~/llvm-repo/build/bin/$1 $2
